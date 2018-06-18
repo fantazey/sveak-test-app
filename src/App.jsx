89911f13311ec39ee5a3194e4b904e561c955b44
Dyatlov Andrey <fantazey@gmail.com>
2018-06-18 07:45:52 +0700

    add webpackconfig

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {hot} from 'react-hot-loader';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return <div className={"App"}>2zxc</div>
+    };
+}
+
+export default hot(module)(App);
